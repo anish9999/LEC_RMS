f6a6e2a7d7b335bc0a720bdde81c7d6767e3e1e0
Anish <anishshah9876@gmail.com>
2022-03-18 06:53:16 +0545

    first commit

--- LEC_Result_Management_System/LEC_Result_Management_System/Staff_Views.py
+++ LEC_Result_Management_System/LEC_Result_Management_System/Staff_Views.py
@@ -1,1 +1,46 @@
 from django.shortcuts import render,redirect
+from LEC_RMSapp.models import Staff,Staff_Notification,FeedBackStaffs
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+
+@login_required(login_url='/')
+def HOME(request):
+    return render(request,'Staff/home.html')
+
+@login_required(login_url='/')
+def NOTIFICATIONS(request):
+    staff = Staff.objects.filter(admin = request.user.id)
+    for i in staff:
+        staff_id = i.id
+
+        notification = Staff_Notification.objects.filter(staff_id = staff_id)
+
+        context = {
+            'notification':notification,
+        }
+        return render(request,'Staff/notification.html',context)
+
+@login_required(login_url='/')
+def STAFF_NOTIFICATION_MARK_AS_DONE(request,status):
+    notification = Staff_Notification.objects.get(id = status)
+    notification.status = 1
+    notification.save()
+    return redirect('notifications')
+
+
+
+def staff_feedback(request):
+    staff_obj = Staff.objects.get(admin=request.user.id)
+    feedback_data = FeedBackStaffs.objects.filter(staff_id=staff_obj)
+    context = {
+        "feedback_data":feedback_data
+    }
+    return render(request, "staff_template/staff_feedback_template.html", context)
+
+
+
+
+
+def STAFF_FEEDBACK(request):
+    return render(request,'Staff/feedback.html')
